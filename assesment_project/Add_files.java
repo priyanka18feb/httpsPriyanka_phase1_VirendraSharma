e46c50ae9c099ea00ae6412cb385a893decfe44f
priyanka18feb <79846969+priyanka18feb@users.noreply.github.com>
2021-03-04 13:38:43 +0530

    Update Add_files.java

--- assesment_project/Add_files.java
+++ assesment_project/Add_files.java
@@ -4,29 +4,40 @@ import java.io.IOException;
 import java.util.Scanner;
 public class Add_files {
 	
-
+	static String path;//defining(not initializing) these variables outside main 
+    static String filename;
 public void add_files()
 	{
-	 // public static void main(String[] args) {
+	
 	File_operation obj = new File_operation();
-	    	Scanner scanner = new Scanner( System.in );
-	        System.out.println("Enter the file name with path: ");
-	        String dirname = scanner.nextLine(); 
+	    	
+	Scanner sc = new Scanner( System.in );
+	 System.out.println("Enter the path : ");
+    path=sc.nextLine(); //storing path in path variable
+      System.out.println("Enter file name : ");
+      filename=sc.nextLine(); //storing filename in filename variable
 	        try {
-	            //File file = new File("d:/sampleFile.txt");
-	        	File file = new File(dirname);
-	       
-	            if(file.createNewFile())
-	                System.out.println("File creation successfull");
-	            else
-	                System.out.println("Error while creating File, file already exists in specified path");
+	        	  // Create an object of the File class 
+	   	     // Replace the file path with path of the directory 
+	   	     File directory = new File(path); 
+	   	        File file = new File(path+"/"+filename);
+
+	   	        if (file.createNewFile())
+	   	            System.out.println("    File \""+filename+"\" added to "+ path+"\n");
+	   	        else if(file.exists())
+	   	            System.out.println("    File \""+filename+"\" already exists at "+path +"\n");
+	   	        else
+	   	            System.out.println("Something went wrong. File NOT added\n");
 	            obj.file_operation();
 	        }
 	        catch(IOException io) {
-	            io.printStackTrace();
+	        	System.out.println("not a valid input, plz try again");
+	        	add_files();
 	        }
 	    }
 
 	}
+
+ 
 
  
