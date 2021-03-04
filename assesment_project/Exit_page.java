961200ffb8591701bc54e34b5e39dadfd2bf1b1e
priyanka18feb <79846969+priyanka18feb@users.noreply.github.com>
2021-03-04 13:42:09 +0530

    Update Exit_page.java

--- assesment_project/Exit_page.java
+++ assesment_project/Exit_page.java
@@ -7,20 +7,20 @@ public class Exit_page {
 	{
 		Main a = new Main();
 		Scanner input = new Scanner(System.in);
-		System.out.println("are you sure you want to exit, press Y/y orN/n");
+		System.out.println("Are you sure you want to exit, press Y/y orN/n......");
 		String s = input.nextLine();
 		while(s!=null)
 		{
 		if(s.equals("y") || s.equals("Y"))
 		{
-			System.out.println("successfully exited");
+			System.out.println("Thank You");
 			System.exit(0);
 		}
 		else 
 			if(s.equals("n") || s.equals("N"))
 			a.menu();
 			else
-				System.out.println("not a valid option");
+				System.out.println("Not a valid option, plz try again:");
 		a.menu();
 		
 		}
